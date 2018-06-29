7312b415756c7887b2afe2e86e2642ef72e32f51
Kossivi Esse <kossivi14@gmail.com>
2018-06-29 01:52:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,7 +3,7 @@ function getIt(){
   $("p").on("click", alert("Hey!"));
 }
 function frameIt(){
-  $('img').on("load", $(this).addClass('tasty'))
+  $('img').on("load", function(){$("img").addClass('tasty')})
 }
 function pressIt(){
   $('#typing').on('keydown', function(key){
